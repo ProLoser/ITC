33e337bcd21581928f92137d55fe442f8bd185dd
Jaime Hernandez <jaime@jaimehernandez.com>
2010-04-16 02:22:01 -0700

    Added debug kit.

--- app_controller.php
+++ app_controller.php
@@ -1,36 +1,37 @@
 <?php
 class AppController extends Controller {
-	
+
 	var $components = array(
-		'Auth', 
+		'Auth',
 		'Session',
+		'DebugKit.Toolbar'
 	);
-	
+
 	var $helpers = array(
 		'UploadPack.Upload',
 		'Session',
 		'Form'
 	);
-	
+
 	function beforeFilter() {
 		$this->__configureAuth();
 		App::import('Model', 'User');
 		User::store($this->Auth->user());
 	}
-	
-	function beforeRender() {		
+
+	function beforeRender() {
 		// Configure Layout
 		if ($this->_prefix()) {
 			$this->layout = 'admin';
 		}
-		
+
 		// Load common layout variables
 		$this->loadModel('User');
 		$popUsers = $this->User->find('list', array('limit' => 10));
 		$reviewCount = $this->User->Review->find('count');
 		$this->set(compact('popUsers', 'reviewCount'));
 	}
-	
+
 	/**
 	 * Checks to see if the current user is the owner of the record
 	 */
@@ -41,22 +42,22 @@ class AppController extends Controller {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Checks to see if the current user is a subscriber
 	 */
 	function _subscriber($id) {
-		$results = $this->{$this->modelClass}->Subscriber->find('first', 
+		$results = $this->{$this->modelClass}->Subscriber->find('first',
 			array('conditions' => array(
-				'foreign_model' => $this->modelClass, 
-				'foreign_id' => $id, 
+				'foreign_model' => $this->modelClass,
+				'foreign_id' => $id,
 				'user_id' => $this->Auth->user('id')
 			))
 		);
 		$this->set('subscriber', $results);
 		return $results;
 	}
-	
+
 	/**
 	 * Checks to see what the current prefix in use is. Checks for 'admin' by
 	 * default.
@@ -70,7 +71,7 @@ class AppController extends Controller {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Configures the AuthComponent according to the application's settings
 	 *
@@ -82,17 +83,18 @@ class AppController extends Controller {
 		$this->Auth->loginAction = array('plugin' => null, 'admin' => false, 'controller' => 'users', 'action' => 'login');
 		$this->Auth->logoutRedirect = '/';
 		$this->Auth->loginRedirect = '/';
-		
+
 		if ($this->_prefix()) {
 			$this->Auth->deny();
 			if ($this->Auth->user('role') != 'Admin') {
 				$this->Session->setFlash('You must be an Admin to access this area');
 				$this->redirect($this->Auth->loginAction);
 			}
-		} else {				
+		} else {
 			$this->Auth->allow();
 			$this->Auth->deny(array('add', 'edit', 'delete'));
 		}
 	}
 }
 ?>
+
