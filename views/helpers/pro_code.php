b177b58339caacb2f0abccaa366d1e561f6e2307
Dean Sofer <proloser@hotmail.com>
2010-04-16 18:11:01 -0700

    Fixed bug with helper and subscription ordering

--- views/helpers/pro_code.php
+++ views/helpers/pro_code.php
@@ -1,6 +1,9 @@
 <?php
 date_default_timezone_set('America/Los_Angeles'); 
 class ProCodeHelper extends AppHelper {
+
+	var $helpers = array('Session', 'Html');
+	
 	function calculateAge($birthday){ 
 		return floor((time() - strtotime($birthday))/31556926);
 	}
