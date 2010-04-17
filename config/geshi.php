0fb1d1f30aec3bb165d75b3f0487bcf905dc17f4
Jaime Hernandez <jaime@jaimehernandez.com>
2010-04-16 23:18:36 -0700

    Changed the config to include another div for use in styling.

--- config/geshi.php
+++ config/geshi.php
@@ -1,6 +1,6 @@
 <?php
 
-$geshi->set_header_type(GESHI_HEADER_NONE);
+$geshi->set_header_type(GESHI_HEADER_PRE_VALID);
 $geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 2);
 $geshi->enable_classes();
 $geshi->set_tab_width(4);
