47dcbeec5b033e28c114db977b5502aa4c0a7407
Dean Sofer <proloser@hotmail.com>
2010-04-17 00:15:30 -0700

    Made checking for user_id in comment points optional

--- models/comment.php
+++ models/comment.php
@@ -66,8 +66,12 @@ class Comment extends AppModel {
 			$this->User->grantPoints('create-comment', $this->data['Comment']['user_id'], $this->data['Comment']['id']);
 		} else {
 			if (isset($this->data['Comment']['owner_vote'])) {
-				$userId = $this->field('user_id', array('Comment.id' => $this->data['Comment']['id']));
-				$this->User->grantPoints('owner-comment-up', $ownerId, $this->data['Comment']['id']);
+				if (!isset($this->data['Comment']['owner_vote'])) {
+					$userId = $this->field('user_id', array('Comment.id' => $this->data['Comment']['id']));
+				} else {
+					$userId = $this->data['Comment']['owner_vote'];
+				}
+				$this->User->grantPoints('owner-comment-up', $userId, $this->data['Comment']['id']);
 			}
 		}
 	}
