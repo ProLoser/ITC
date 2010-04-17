d639a3a25d20402e99f350914e99f8b9bc50cbc0
Jaime Hernandez <jaime@jaimehernandez.com>
2010-04-17 01:51:49 -0700

    Css updated and fixed an error with geshi.

--- views/helpers/geshi.php
+++ views/helpers/geshi.php
@@ -4,16 +4,19 @@ App::import('Vendor', 'geshi/geshi');
 class GeshiHelper extends AppHelper {
 
 	function parse($code, $lang, $header = null) {
+		strtolower($lang);
 		$geshi = new GeSHI(trim($code), $lang);
 		//Set Geshi configuration settings
 		if (file_exists(CONFIGS.'geshi.php')) {
 			include CONFIGS.'geshi.php';
 		}
+		$geshi->language_data['LANG_NAME'] = $lang;
 		if ($header) $geshi->set_header_content($header);
 		return $geshi->parse_code();
 	}
 
 	function parse_highlight($code, $lang, $start, $end) {
+		strtolower($lang);
 		$geshi = new GeSHI(trim($code), $lang);
 		//Set Geshi configuration settings
 		if (file_exists(CONFIGS.'geshi.php')) {
@@ -23,6 +26,7 @@ class GeshiHelper extends AppHelper {
 		for($i = 0; $i <= ($end - $start); $i++) {
 			$lines[$i] = (String)$start+$i;
 		}
+		$geshi->language_data['LANG_NAME'] = $lang;
 		$geshi->highlight_lines_extra($lines);
 		return $geshi->parse_code();
 	}
